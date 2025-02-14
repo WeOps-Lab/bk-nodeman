f000371eb8eb5acd566fdc7e13cb770356b4c941
zhongming Fan <zhongmingfan@canway.net>
2025-02-14 10:39:11 +0800

    fix: windows服务器解压后占用文件夹无法move

--- script_tools/plugin_scripts/update_binary.bat
+++ script_tools/plugin_scripts/update_binary.bat
@@ -133,6 +133,7 @@ rem 拷贝插件脚本到官方插件目录下,避免脚本老旧有bug或者不
 if "%SWWIN_TARGET_DIR%"=="external" if defined GROUP_DIR (
     rem 第三方插件指定了group_id，解压后需要将插件从标准路径移动到实例路径下
     rd /S /Q %WIN_GSE_BINDIR%
+    timeout /t 5
     move %SWWIN_GSE_HOME%\external_plugins\%SWWIN_PLUGIN_NAME% %WIN_GSE_BINDIR%\..\
 )
 
